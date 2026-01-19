7084c3e2559931f7c070c06495eaff305cf62261
TLN18 <Goldengamer2026@gmail.com>
2026-01-19 13:29:11 -0600

    Add files via upload

---
+++ site.js
@@ -0,0 +1,5 @@
+const p = document.querySelector('p')
+        
+        setTimeout(() => {
+            p.textContent = "Hi! I'm TJ, and this is my personal homepage." + "Welcome!"
+        }, 3000)
