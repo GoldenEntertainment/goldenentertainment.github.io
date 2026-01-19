12a682b41c99958024037a2510f3c7824a267d05
TLN18 <Goldengamer2026@gmail.com>
2026-01-19 13:31:20 -0600

    Fix text content in site.js welcome message

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 const p = document.querySelector('p')
         
         setTimeout(() => {
-            p.textContent = "Hi! I'm TJ, and this is my personal homepage." + "Welcome!"
+            p.textContent = "Hi! I'm TJ, and this is my personal homepage." + " - Welcome!"
+
         }, 3000)
