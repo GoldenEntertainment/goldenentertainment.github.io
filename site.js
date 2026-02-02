3459b35dd1e2bf2af2afaef944f7a3801ce51783
TLN18 <Goldengamer2026@gmail.com>
2026-02-02 16:18:25 -0600

    Added secret message

--- site.js
+++ site.js
@@ -4,3 +4,9 @@ const p = document.querySelector('p')
             p.textContent = "Hi! I'm TJ, and this is my personal homepage." + " - Welcome!"
 
         }, 3000)
+
+localStorage.setItem('visited', 'true');
+
+if (!localStorage.getItem('visited')) {
+    p.textContent = "It's a secret to everybody. if you see this, know that i like pizza and wings!";
+}
